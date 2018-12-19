ddfb16c41d3c63b83dc728d666db3245baf25745
Monika Zietal <monika.zietal@futurum.tech>
2018-12-19 13:06:30 +0100

    Add sidebar

--- src/App.js
+++ src/App.js
@@ -1,12 +1,31 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { Component } from "react";
+import Sidebar from "react-sidebar";
+import "./App.css";
 
 class App extends Component {
+  state = {
+    sidebarOpen: true
+  };
+
+  onSetSidebarOpen = this.onSetSidebarOpen.bind(this);
+
+  onSetSidebarOpen(open) {
+    this.setState({ sidebarOpen: open });
+  }
+
   render() {
     return (
       <div className="app">
-        
+        <Sidebar
+          sidebar={<b>Sidebar content</b>}
+          open={this.state.sidebarOpen}
+          onSetOpen={this.onSetSidebarOpen}
+          styles={{ sidebar: { background: "white" } }}
+        >
+          <button onClick={() => this.onSetSidebarOpen(true)}>
+            Open sidebar
+          </button>
+        </Sidebar>
       </div>
     );
   }

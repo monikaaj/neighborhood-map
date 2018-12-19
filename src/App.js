fceff8b61504e2acc4d38ef6d31a5279b0740153
Monika Zietal <monika.zietal@futurum.tech>
2018-12-19 13:12:16 +0100

    Responsive sidebar

--- src/App.js
+++ src/App.js
@@ -2,15 +2,31 @@ import React, { Component } from "react";
 import Sidebar from "react-sidebar";
 import "./App.css";
 
+const mql = window.matchMedia(`(min-width: 800px)`);
+
 class App extends Component {
   state = {
+    sidebarDocked: mql.matches,
     sidebarOpen: true
   };
 
+  mediaQueryChanged = this.mediaQueryChanged.bind(this);
   onSetSidebarOpen = this.onSetSidebarOpen.bind(this);
+
+  componentWillMount() {
+    mql.addListener(this.mediaQueryChanged);
+  }
+ 
+  componentWillUnmount() {
+    this.state.mql.removeListener(this.mediaQueryChanged);
+  }
 
   onSetSidebarOpen(open) {
     this.setState({ sidebarOpen: open });
+  }
+
+  mediaQueryChanged() {
+    this.setState({ sidebarDocked: mql.matches, sidebarOpen: false });
   }
 
   render() {
@@ -20,6 +36,7 @@ class App extends Component {
           sidebar={<b>Sidebar content</b>}
           open={this.state.sidebarOpen}
           onSetOpen={this.onSetSidebarOpen}
+          docked={this.state.sidebarDocked}
           styles={{ sidebar: { background: "white" } }}
         >
           <button onClick={() => this.onSetSidebarOpen(true)}>

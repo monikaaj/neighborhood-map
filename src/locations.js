962e0bc7167bc50e6d476cbc2fba775c62e3583a
Monika Zietal <monika.zietal@futurum.tech>
2018-12-20 12:11:44 +0100

    Add locations data

---
+++ src/locations.js
@@ -0,0 +1,42 @@
+const locations = [
+    {
+        title: "Rynek Główny",
+        location: {
+            lat: 50.061527,
+            lng: 19.937959
+        },
+        venueId: "4b6db00cf964a5207c862ce3"
+    },
+    {
+        title: "Planty",
+        location: {
+            lat: 50.064118,
+            lng: 19.934437
+        },
+        venueId: "4b81156ef964a520169630e3"
+    },
+    {
+        title: "Wawel Castle",
+        location: {
+            lat: 50.054060,
+            lng: 19.935416
+        },
+        venueId: "4b8128ccf964a520ba9830e3"
+    },
+    {
+        title: "St. Mary‘s Basilica",
+        location: {
+            lat: 50.061668,
+            lng: 19.939268
+        },
+        venueId: "4cfa481bee9cb60c3d4089ad"
+    },
+    {
+        title: "Błonia",
+        location: {
+            lat: 50.05919,
+            lng: 19.908811
+        },
+        venueId: "4bdab8ca2a3a0f478ec4abb6"
+    }
+]
